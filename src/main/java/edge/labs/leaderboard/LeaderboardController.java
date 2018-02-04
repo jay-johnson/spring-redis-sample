3a3ae30fe9ba0aedf185dc390c93a6a82198b06e
Matt Edge <mattedgeis@gmail.com>
2018-02-04 13:22:58 -0500

    Testing data for UI alterations

--- src/main/java/edge/labs/leaderboard/LeaderboardController.java
+++ src/main/java/edge/labs/leaderboard/LeaderboardController.java
@@ -1,11 +1,14 @@
 package edge.labs.leaderboard;
 
+import edge.labs.leaderboard.score.Score;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Collections;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/leaderboards")
@@ -13,7 +16,15 @@ public class LeaderboardController {
 
     @GetMapping("/")
     public String leaderboardsPage(Model model) {
-        model.addAttribute("leaderboards", Collections.emptyList()); // TODO
+
+        List<Score> scores = Arrays.asList(
+            new Score("medge", 1, LocalDateTime.now().minusDays(11)),
+            new Score("tom", 42, LocalDateTime.now().minusDays(4)),
+            new Score("ashley", 61255, LocalDateTime.now().minusDays(1))
+        );
+        Leaderboard test = new Leaderboard("EdgeLabs", scores);
+
+        model.addAttribute("leaderboards", Arrays.asList(test)); // TODO
         return "index";
     }
 
